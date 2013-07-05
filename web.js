eabdbafc4dcb7d4742699832f7871d7365d2a8cd
Igor Pinotti <igorpinotti@gmail.com>
2013-07-05 21:27:21 +0000

    add read index

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
-
+var fs = require ('fs');
 var app = express.createServer(express.logger());
+var buffer = new buffer(48);
+var path = "index.html";
+
+buffer = fs.readFileSync(path, function (err, stats) {
+  if (err) throw err;
+  console.log('stats: ' + JSON.stringify(stats));
+});
 
 app.get('/', function(request, response) {
-  response.send('My World!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
