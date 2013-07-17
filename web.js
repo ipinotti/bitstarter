9463637397ae3d933c72086d032b47b066950ee4
Igor Pinotti <igorpinotti@gmail.com>
2013-07-17 14:45:44 +0000

    change port to 8080

--- web.js
+++ web.js
@@ -5,17 +5,11 @@ var path = './index.html';
 var buffer = fs.readFileSync(path);
 console.log ("Read File ok"); 
 
-//fs.readFile('./index.html', function (err, data) {
-//  if (err) throw err;
-//  console.log(data);
-//  buffer = data;
-//});
-
 app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
